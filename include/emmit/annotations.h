8868b3bd339b22a5af944bb29d598bb70f556a0a
desmonHak <61366533+desmonHak@users.noreply.github.com>
2026-03-28 04:53:27 +0100

    añadiendo notaciones con efectos al ensamblador. Ahora tenemos la primera libreria con definiciones formales de los espacios de memoria de la VM por defecto. Ademas hemos implementado el apartado de secciones y ya realizamos su analisis

---
+++ include/emmit/annotations.h
@@ -0,0 +1,8 @@
+//
+// Created by desmon0xff on 28/03/2026.
+//
+
+#ifndef ANNOTATIONS_H
+#define ANNOTATIONS_H
+
+#endif //ANNOTATIONS_H
---
+++ src/emmit/annotations.cpp
@@ -0,0 +1,3 @@
+//
+// Created by desmon0xff on 28/03/2026.
+//
