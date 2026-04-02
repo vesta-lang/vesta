8622a61869b5ac3192796ce31bd88c6d6cb2212b
desmonHak <61366533+desmonHak@users.noreply.github.com>
2026-04-03 01:05:13 +0200

    añadiendo utilidades para ensamblado de archivos multiples usando multiproceso

---
+++ src/util/assembler_multiprocess.cpp
@@ -0,0 +1,22 @@
+/**
+ * @file fs_utils.h
+ * @brief Utilidades portables para comprobación de rutas y permisos (Windows / POSIX).
+ *
+ * VestaVM - Máquina Virtual Distribuida
+ *
+ * Copyright © 2026 David López.T (DesmonHak)
+ * Licencia VMProject
+ *
+ * USO LIBRE NO COMERCIAL con atribución obligatoria.
+ *
+ * Este fichero contiene utilidades ligeras basadas en std::filesystem para:
+ *  - comprobar existencia de rutas
+ *  - normalizar rutas
+ *  - comprobar permisos de lectura/escritura (heurístico en Windows)
+ *
+ * @note Requiere compilador con soporte para <filesystem> (C++17/C++20).
+ */
+#ifndef ASSEMBLER_MULTIPROCESS_H
+#define ASSEMBLER_MULTIPROCESS_H
+
+#endif //ASSEMBLER_MULTIPROCESS_H
